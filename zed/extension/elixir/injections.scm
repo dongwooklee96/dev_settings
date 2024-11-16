928e8166cd386b6abb5059943f04e78d2e2da30b
DongWook Lee <sh95119@gmail.com>
2024-11-17 01:24:35 +0900

    init

---
+++ zed/extension/elixir/injections.scm
@@ -0,0 +1,14 @@
+; Phoenix HTML template
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @content)
+ (#eq? @_sigil_name "H")
+ (#set! language "heex")
+ )
+
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @content)
+ (#eq? @_sigil_name "V")
+ (#set! language "svelte")
+)
